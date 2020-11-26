87a2f44f48eb90cab6fb2209d0ced1a1dda8fa63
littlechameleon <1187307315@qq.com>
2020-11-27 01:11:17 +0800

    exercise vue3

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import anichart from "anichart";
+export default class About extends Vue {
+  async onMounted() {
+    console.log("222")
+    const chart = new anichart.Bar();
+    await chart.loadCsv("./data.csv");
+    chart.initCanvas();
+    chart.readyToDraw();
+  }
+}
+</script>
